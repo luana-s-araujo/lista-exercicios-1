96d56019db0c93cd941d717d69d8971ee75f3a23
Luana Araújo <93063594+luana-s-araujo@users.noreply.github.com>
2021-11-06 22:24:55 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,89 @@
+#Para testar qualquer questão basta tirar do modo comentário
+#-------------------------------------------------------------------
+
+#Q1
+#n1 = int(input("Digite aqui um número inteiro: "))
+#n2 = int(input("Digite aqui outro número inteiro: "))
+#print(n1+n2)
+
+
+#Q2
+#valor_em_metros = float(input("Digite aqui o valor em metros a ser convertido para milímetros: "))
+#print(valor_em_metros / 1000)
+
+
+#Q3
+#dias = int(input("Digite aqui a quantidade de dias: "))
+#horas = int(input("Digite aqui a quantidade de horas: "))
+#minutos =  int(input("Digite aqui a quantidade de minutos: "))
+#segundos = int(input("Digite aqui a quantidade de segundos: "))
+#print(segundos + (minutos/60) + (horas/3600) + (dias*86400))
+
+
+#Q4
+#salario = float(input("Digite aqui seu salário atual: "))
+#aumento_em_porcentagem = float(input("Digite aqui o #valor do aumento em porcentagem (sem o símbolo %): "))
+#aumento = salario * aumento_em_porcentagem / 100
+#novo_salario = (salario + aumento)
+#print("O aumento foi de", aumento)
+#print("Novo salário agora é de", novo_salario)
+
+
+#Q5
+#valor_do_produto = float(input("Digite aqui o valor do produto: "))
+#desconto = float(input("Digite aqui o percentual do desconto (sem o %): "))
+#valor_do_desconto = desconto * valor_do_produto / 100
+#preco_a_pagar = valor_do_produto - desconto
+#print("O desconto foi de: ", desconto)
+#print("O total a pagar é de: ", preco_a_pagar)
+
+
+#Q6
+#distancia_total = float(input("Digite aqui a distância total da viajem em kilômetros: "))
+#velocidade_media = float(input("Digite aqui a velocidade média da viajem: "))
+#tempo = velocidade_media / distancia_total
+#print("O tempo aproximdado da viajem em horas foi de: ", tempo#)
+
+
+#Q7 e Q8 - Resolvidos na oficina e disponíveis no GitHub
+#------------------------------------------------------------------------
+# Converta uma temperatura digitada em Celsius para
+# Fahrenheit. F = 9*C/5 + 32
+
+#celsius = input("Por favor, insira a temperatura em graus Celsius: ")
+#fahrenheit = 9 * float(celsius) / 5 + 32
+#print("A temperatura em Fahrenheit é: %f" % conversao)
+
+# Faça agora o contrário, de Fahrenheit para Celsius
+#c = (fahrenheit - 32) * 5/9
+#print(f"A temperatura em Celsius é: {c}")
+#-------------------------------------------------------------------------
+
+
+#Q9
+#distancia_percorrida = float(input("Digite aqui a distância percorrida em km do seu carro alugado: "))
+#dias_alugado = float(input("Digite aqui a quantidade de dias que ficou com o carro: "))
+#preco_a_pagar = (distancia_percorrida*0.15) + (dias_alugado*60)
+#print("O total a pagar foi de: ", preco_a_pagar)
+
+
+#Q10
+#cigarros_dia = int(input("Digite aqui a quantidade de cigarros fumados por dia: "))
+#anos_de_vicio = int(input("Digite aqui a quantidade de tempo (em anos) de uso do cigarro: "))
+#minutos = anos_de_vicio * 365 * cigarros_dia * 10
+#dias = minutos / (24*60)
+#print("Tempo de dias reduzido: ", dias)
+
+
+#Q11
+#print ('2 elevado à 1 milhão é %d ' %len(str(2**1000000)) )
+
+
+
+
+
+
+
+
+
+
